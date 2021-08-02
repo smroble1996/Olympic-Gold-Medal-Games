354e644e54abb15b4ed427b847b3fea3237c5c2d
Sam Shearer <sshearer101@gmail.com>
2021-08-02 16:13:11 -0600

    third

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const scheduleTitle = document.getElementById('schedule-title')
 const scheduleButton = document.getElementById('schedule-button')
-const schedule = document.getElementById('schedule')
+const finalSchedule = document.getElementById('final-schedule')
+const sportTitle = document.getElementById('sport-title')
+const sportImage = document.getElementById('sport-image')
+const commentForm = document.getElementById('commentForm')
+const commentButton = document.getElementById('commentButton')
 
-fetch ("http://localhost:3000")
+fetch ("http://localhost:3000/teams")
 .then(res => res.json())
 .then(renderData)
 
 
 scheduleButton.addEventListener('click', () => {
-    schedule.innerText = `August ${day} 2021`
+    renderSchedule()
 })
+
+// commentButton.addEventListener('submit', (e) =>{
+//     e.preventDefault()
+// })
 
 
 
 function renderData(data){
 
-    scheduleTitle.innerText = data.title
+    sportTitle.innerText = data[3].title
 
 
+}
 
-
-
+function renderSchedule(day){
+    finalSchedule.innerText = `August ${day} 2021`
 }
